13ee6a65ee3e5c61e105003686d2b496bd1d07cf
H0nok4 <704017380@qq.com>
2023-11-14 12:57:42 +0800

    assets:提交一下

--- TurnBaseRPG/Assets/Scripts/Gameplay/Player/PlayerObject.cs
+++ TurnBaseRPG/Assets/Scripts/Gameplay/Player/PlayerObject.cs
@@ -6,6 +6,29 @@ public class PlayerObject : MonoBehaviour
 {
 
     public void Update() {
+        //TODO:后面需要一个专门的地方处理输入
+        HandleMove();
+    }
 
+    public void HandleMove() {
+        if (Input.GetKeyDown(KeyCode.W)) {
+            //TODO:向上移动
+
+        }
+        else if (Input.GetKeyDown(KeyCode.D)) {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.S)) {
+
+        }
+        else if (Input.GetKeyDown(KeyCode.A)) {
+
+        }
+    }
+
+    public void HandleInteract() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            
+        }
     }
 }
