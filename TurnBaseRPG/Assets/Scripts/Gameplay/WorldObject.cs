1e06c51208d9d532f791f1fb9a979a9355a9c0db
H0nok4 <704017380@qq.com>
2023-11-16 13:00:53 +0800

    assets:WorldObject修改

--- TurnBaseRPG/Assets/Scripts/Gameplay/WorldObject.cs
+++ TurnBaseRPG/Assets/Scripts/Gameplay/WorldObject.cs
@@ -2,8 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class WorldObject : IInteractive
-{
+public abstract class WorldObject : MonoBehaviour, IInteractive {
+    public Transform TransformComponent;
+
+    public Collider2D ColliderComponent;
+
+    private void Start() {
+        TransformComponent = GetComponent<Transform>();
+        ColliderComponent = GetComponent<Collider2D>();
+        
+        InitObject();
+    }
+
+    public virtual void InitObject() {
+
+    }
+
     public virtual void OnTrigger() {
         
     }
