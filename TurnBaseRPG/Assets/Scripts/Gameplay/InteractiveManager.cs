a622e1de63a751fb07b77c5df746d70fa2ac4320
H0nok4 <704017380@qq.com>
2023-11-23 19:03:27 +0800

    assets:互动雏形

--- TurnBaseRPG/Assets/Scripts/Gameplay/InteractiveManager.cs
+++ TurnBaseRPG/Assets/Scripts/Gameplay/InteractiveManager.cs
@@ -6,6 +6,25 @@ public class InteractiveManager : MonoSingleton<InteractiveManager> {
     public List<IInteractive> InteractiveObject = new List<IInteractive>();
 
     public void Start() {
-
+        EventManager.Instance.AddListener<IInteractive>(EventDef.PlayerEnterNPCTrigger, AddNPCInteract);
+        EventManager.Instance.AddListener<IInteractive>(EventDef.PlayerExitNPCTrigger,RemoveNPCInteract);
     }
+
+    private void AddNPCInteract(IInteractive obj) {
+        if (InteractiveObject.Contains(obj)) {
+            return;
+        }
+        InteractiveObject.Add(obj);
+        Debug.Log($"增加一个互动弹窗,当前玩家靠近了{InteractiveObject.Count}个可互动物体");
+    }
+
+    private void RemoveNPCInteract(IInteractive obj) {
+        if (!InteractiveObject.Contains(obj)) {
+            return;
+        }
+
+        InteractiveObject.Remove(obj);
+        Debug.Log($"移除一个互动弹窗,当前玩家靠近了{InteractiveObject.Count}个可互动物体");
+    }
+
 }
