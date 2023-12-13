f12108cae8bedb097f29f86d7203a965c6c55dac
H0nok4 <704017380@qq.com>
2023-12-13 18:25:06 +0800

    assets:准备做互动界面

--- TurnBaseRPG/Assets/Scripts/Base/UI/Panel/MainPanel.cs
+++ TurnBaseRPG/Assets/Scripts/Base/UI/Panel/MainPanel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UI;
+using UnityEngine;
 
 namespace Assets.Scripts.Base.UI.Panel {
     public class MainPanel : UIPanel {
@@ -21,6 +22,9 @@ namespace Assets.Scripts.Base.UI.Panel {
         private void OnAddInteract() {
             ListInteract.AddChild();
             //ListInteract.gameObject.transform.position = PlayerObject
+            //var findPlayer = GameObject.Find("PlayerObject").GetComponent<PlayerObject>();
+            ListInteract.gameObject.SetActive(true);
+      
         }
 
         private void OnRemoveInteract() {
